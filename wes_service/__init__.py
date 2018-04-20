663bae21d17a5fc589dc47c7c4098dcb3013ba84
David Steinberg <david@resium.com>
2018-04-20 12:58:47 -0700

    Add debug feature

--- wes_service/__init__.py
+++ wes_service/__init__.py
@@ -19,6 +19,7 @@ def main(argv=sys.argv[1:]):
     parser.add_argument("--backend", type=str, default="wes_service.cwl_runner")
     parser.add_argument("--port", type=int, default=8080)
     parser.add_argument("--opt", type=str, action="append")
+    parser.add_argument("--debug", action="store_true", default=False)
     args = parser.parse_args(argv)
 
     app = connexion.App(__name__)
@@ -28,7 +29,7 @@ def main(argv=sys.argv[1:]):
 
     app.add_api('openapi/workflow_execution_service.swagger.yaml', resolver=Resolver(rs))
 
-    app.run(port=args.port)
+    app.run(port=args.port, debug=args.debug)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
