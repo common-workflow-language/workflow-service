5e3cc2683e4f4f2b47c5d9c02681083f30c27b4f
DailyDreaming <lblauvel@ucsc.edu>
2018-07-10 12:33:55 -0700

    Docstrings to util.

--- wes_service/util.py
+++ wes_service/util.py
@@ -1,27 +1,34 @@
+from six import itervalues
+
 def visit(d, op):
+    """Recursively call op(d) for all list subelements and dictionary 'values' that d may have."""
     op(d)
     if isinstance(d, list):
         for i in d:
             visit(i, op)
     elif isinstance(d, dict):
-        for i in d.itervalues():
+        for i in itervalues(d):
             visit(i, op)
 
 
 class WESBackend(object):
+    """Stores and retrieves options.  Intended to be inherited."""
     def __init__(self, opts):
+        """Parse and store options as self.pairs."""
         self.pairs = []
         for o in opts if opts else []:
             k, v = o.split("=", 1)
             self.pairs.append((k, v))
 
     def getopt(self, p, default=None):
+        """Returns the first option value stored that matches p or default."""
         for k, v in self.pairs:
             if k == p:
                 return v
         return default
 
     def getoptlist(self, p):
+        """Returns all option values stored that match p as a list."""
         optlist = []
         for k, v in self.pairs:
             if k == p:
