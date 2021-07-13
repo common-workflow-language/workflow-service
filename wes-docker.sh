d6d5bafe4d7b9d13a9209e6be820fada4b6bfb71
Michael R. Crusoe <michael.crusoe@gmail.com>
2021-07-13 11:50:24 +0200

    Dockerfile: finish stretch→buster upgrade, python3

--- wes-docker.sh
+++ wes-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-set -e
+set -ex
 python setup.py sdist
-docker build --build-arg version=2.3 -t commonworkflowlanguage/workflow-service .
-docker run -ti \
+docker build --build-arg version=4.0 --build-arg arvversion=2.2.1 -t commonworkflowlanguage/workflow-service .
+docker run -ti -p 127.0.0.1:3000:3000/tcp \
        -v$PWD/config.yml:/var/www/wes-server/config.yml \
        -v/etc/ssl/certs/ssl-cert-snakeoil.pem:/etc/ssl/certs/ssl-cert-wes.pem \
        -v/etc/ssl/private/ssl-cert-snakeoil.key:/etc/ssl/private/ssl-cert-wes.key \
